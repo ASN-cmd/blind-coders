2bcd867d5bf8ebda2fabefe1ee0892745feafda4
Tanmay Chavan <tanmaychavan2214@gmail.com>
2026-03-13 00:08:19 +0530

    download script for embedding model

--- backend/embeddings/embedding_model.py
+++ backend/embeddings/embedding_model.py
@@ -1,9 +1,12 @@
 from sentence_transformers import SentenceTransformer
 
 _embedder = None
-
+BASE_DIR = Path(__file__).resolve().parents[2]
+MODEL_PATH = BASE_DIR / "models" / "all-MiniLM-L6-v2"
 def load_embedding_model():
     global _embedder
+
     if _embedder is None:
-        _embedder = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+        _embedder = SentenceTransformer(str(MODEL_PATH))
+
     return _embedder
